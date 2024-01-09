82617868f16b91337d1a2996920154d9561fa20f
USR\juliana.santa <juliana.santa@fleetcor.com.br>
2024-01-09 18:13:07 -0300

    Iteration#3

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -101,3 +101,50 @@ globalBasePrice = 25000
 car1.finalPrice = car1.basePrice + globalBasePrice
 car2.finalPrice = car2.basePrice + globalBasePrice
 console.log(car1.finalPrice, car2.finalPrice);
+
+/* 
+**Iteración #3: Operadores**
+
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+//? 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+//!--------------------------------------------------------------------------------------------------------
+console.log(10 * 5);
+
+//!--------------------------------------------------------------------------------------------------------
+//? 1.2 Divide 10 por 2 y muestra el resultado en un console.
+//!--------------------------------------------------------------------------------------------------------
+console.log(10 / 2);
+
+//!--------------------------------------------------------------------------------------------------------
+//? 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+//!--------------------------------------------------------------------------------------------------------
+console.log(15 % 9);
+
+//!--------------------------------------------------------------------------------------------------------
+//? 1.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
+//!--------------------------------------------------------------------------------------------------------
+p = 10
+j = 5
+o = p + j
+console.log(o);
+
+//!--------------------------------------------------------------------------------------------------------
+//? 1.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
+//!--------------------------------------------------------------------------------------------------------
+c = 10
+m = 5
+i = c * m
+console.log(i);
