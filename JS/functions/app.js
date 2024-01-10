4936474d16a02f4b6e272900a757490a54faa134
USR\juliana.santa <juliana.santa@fleetcor.com.br>
2024-01-10 08:53:29 -0300

    Iteration#5

--- JS/functions/app.js
+++ JS/functions/app.js
@@ -151,3 +151,40 @@ function average(param) {
 }
 
 console.log(average(numbers))
+
+/* 
+**Iteración #5: Calcular promedio de strings**
+
+Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:
+
+const mixedElements = [6, 1, "Rayo", 1, "vallecano", "10", "upgrade", 8, "hub"];
+function averageWord(param) {
+  insert code
+}
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #5: Calcular promedio de strings**
+
+ * ? Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:
+ * ? const mixedElements = [6, 1, "Rayo", 1, "vallecano", "10", "upgrade", 8, "hub"];
+ * ? function averageWord(param) {
+ * ?   insert code
+ * ? }
+ */
+//!--------------------------------------------------------------------------------------------------------
+const mixedElements = [6, 1, "Rayo", 1, "vallecano", "10", "upgrade", 8, "hub"];
+
+function averageWord(param) {
+  return param.reduce((acc, el) => {
+    if (typeof el == "number") {
+      acc += el
+    } else {
+      acc += el.length
+    }
+    return acc
+  }, 0)
+}
+
+console.log(averageWord(mixedElements))
