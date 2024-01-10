e20894f493f6303acfa614c9995db58bd8a8a6c8
USR\juliana.santa <juliana.santa@fleetcor.com.br>
2024-01-10 06:57:36 -0300

    Interation#1

---
+++ JS/functions/app.js
@@ -0,0 +1,21 @@
+/* 
+**Iteración #1: Buscar el máximo**
+
+Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne, numberTwo) {
+  insert code
+}
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #1: Buscar el máximo**
+ * ? Completa la función que tomando dos números como argumento devuelva el más alto.
+ */
+//!--------------------------------------------------------------------------------------------------------
+function sum(numberOne, numberTwo) {
+  console.log(Math.max(numberOne, numberTwo))
+}
+
+sum(10, 5)
