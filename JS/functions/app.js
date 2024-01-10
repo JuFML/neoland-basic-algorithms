2c1b01a5531a8a78dfc360a4e741e0066fd6d6a3
USR\juliana.santa <juliana.santa@fleetcor.com.br>
2024-01-10 08:02:37 -0300

    Iteration#2

--- JS/functions/app.js
+++ JS/functions/app.js
@@ -18,4 +18,63 @@ function sum(numberOne, numberTwo) {
   console.log(Math.max(numberOne, numberTwo))
 }
 
-sum(10, 5)
+sum(7, 5)
+
+/* 
+**Iteración #2: Buscar la palabra más larga**
+
+Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función:
+
+const avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(param) {
+  insert code
+}
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #2: Buscar la palabra más larga**
+ * ? Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+ * 
+ * ? Puedes usar este array para probar tu función:
+ * ? const avengers = [
+ * ?   "Hulk",
+ * ?   "Thor",
+ * ?   "IronMan",
+ * ?   "Captain A.",
+ * ?   "Spiderman",
+ * ?   "Captain M.",
+ * ? ];
+ * ? function findLongestWord(param) {
+ * ?   insert code
+ * ? }
+ */
+//!--------------------------------------------------------------------------------------------------------
+const avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+
+function findLongestWord(param) {
+  return param.reduce((acc, el) => {
+    if (el.length > acc.length) {
+      acc = el
+    }
+    return acc
+  }, "")
+}
+
+console.log(findLongestWord(avengers))
