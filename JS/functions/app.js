fa104baa78d048cf04b1785f0541e8a101ba48ce
USR\juliana.santa <juliana.santa@fleetcor.com.br>
2024-01-10 10:27:13 -0300

    Iteration#7

--- JS/functions/app.js
+++ JS/functions/app.js
@@ -257,3 +257,73 @@ function removeDuplicates(param) {
 }
 
 console.log(removeDuplicates(duplicates))
+
+/* 
+**Iteración #7: Buscador de nombres**
+
+Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
+
+const nameFinder = [
+  "Peter",
+  "Steve",
+  "Tony",
+  "Natasha",
+  "Clint",
+  "Logan",
+  "Xabier",
+  "Bruce",
+  "Peggy",
+  "Jessica",
+  "Marc",
+];
+function finderName(param) {
+  insert code
+}
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #7: Buscador de nombres**
+
+ * ? Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
+ * ? const nameFinder = [
+ * ?   "Peter",
+ * ?   "Steve",
+ * ?   "Tony",
+ * ?   "Natasha",
+ * ?   "Clint",
+ * ?   "Logan",
+ * ?   "Xabier",
+ * ?   "Bruce",
+ * ?   "Peggy",
+ * ?   "Jessica",
+ * ?   "Marc",
+ * ? ];
+ * 
+ * ? function finderName(param) {
+ * ?  insert code
+ * ?}
+ */
+//!--------------------------------------------------------------------------------------------------------
+const nameFinder = [
+  "Peter",
+  "Steve",
+  "Tony",
+  "Natasha",
+  "Clint",
+  "Logan",
+  "Xabier",
+  "Bruce",
+  "Peggy",
+  "Jessica",
+  "Marc",
+];
+
+function finderName(arr, value) {
+  if (arr.find(el => value === el)) {
+    console.log(true, arr.indexOf(value))
+    return
+  }
+  console.log(false)
+}
+finderName(nameFinder, "Peter")
