e2c2f09cad0e40bed32c9e0ba52557358db1041c
JuFML <julianafernandezlima@hotmail.com>
2024-01-11 18:25:13 -0300

    Iteration#1

---
+++ JS/loops/app.js
@@ -0,0 +1,34 @@
+/*
+**Iteración #1: Usa includes**
+
+Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta". Usa la función .**_includes_** de javascript.
+
+const products = [
+  "Camiseta de Pokemon",
+  "Pantalón coquinero",
+  "Gorra de gansta",
+  "Camiseta de Basket",
+  "Cinrurón de Orión",
+  "AC/DC Camiseta",
+];
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #1: Usa includes**
+ * ? Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta". Usa la función .**_includes_** de javascript.
+ */
+//!--------------------------------------------------------------------------------------------------------
+
+const products = [
+  "Camiseta de Pokemon",
+  "Pantalón coquinero",
+  "Gorra de gansta",
+  "Camiseta de Basket",
+  "Cinrurón de Orión",
+  "AC/DC Camiseta",
+];
+
+products.forEach(product => {
+  product.includes("Camiseta") && console.log(product);
+})
