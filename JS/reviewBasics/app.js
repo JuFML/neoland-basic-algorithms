a0269bf454c6d23845d13d18150df32c809013d6
JuFML <julianafernandezlima@hotmail.com>
2024-01-12 18:50:38 -0300

    Iteración#5

--- JS/reviewBasics/app.js
+++ JS/reviewBasics/app.js
@@ -285,3 +285,23 @@ function findArrayIndex(array, text) {
 }
 
 console.log(findArrayIndex(animals, "mosquito"))
+
+/*
+**Iteración #5: Función rollDice**
+
+Crea una función llamada **rollDice()** que reciba como parametro el numero de caras que queramos que tenga el dado que deberá silumar el codigo dentro de la función. Como hemos dicho, que la función use el parametro para simular una tirada de dado y retornar el resultado. Si no se te ocurre como hacer un numero aleatorio no te preocupes! busca información sobre la función de javascript **Math.random();**
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #5: Función rollDice**
+ * ? Crea una función llamada **rollDice()** que reciba como parametro el numero de caras que queramos que tenga el dado que deberá silumar el codigo dentro de la función. Como hemos dicho, que la función use el parametro para simular una tirada de dado y retornar el resultado. Si no se te ocurre como hacer un numero aleatorio no te preocupes! busca información sobre la función de javascript **Math.random();**
+ */
+//!--------------------------------------------------------------------------------------------------------
+
+const rollDice = (numeroCaras) => {
+  return Math.random() * numeroCaras
+}
+
+const diceResult = rollDice(5)
+console.log(diceResult)
