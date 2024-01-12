8a2ca006d05238f5013930b8eb6bee147094132d
JuFML <julianafernandezlima@hotmail.com>
2024-01-12 06:28:59 -0300

    Iteración#5

--- JS/loops/app.js
+++ JS/loops/app.js
@@ -97,7 +97,7 @@ const placesToTravel = [
  */
 //!--------------------------------------------------------------------------------------------------------
 
-const placesToTravel = [
+let placesToTravel = [
   "Japon",
   "Venecia",
   "Murcia",
@@ -140,3 +140,44 @@ const alien = {
 for (const prop in alien) {
   console.log(`${prop}: ${alien[prop]}`);
 }
+
+/*
+**Iteración #5: Probando For**
+
+Usa un bucle **for** para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en un **_console log_** el array. Puedes usar este array:
+
+const placesToTravel = [
+  { id: 5, name: "Japan" },
+  { id: 11, name: "Venecia" },
+  { id: 23, name: "Murcia" },
+  { id: 40, name: "Santander" },
+  { id: 44, name: "Filipinas" },
+  { id: 59, name: "Madagascar" },
+];
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #5: Probando For**
+ * ? Usa un bucle **for** para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en un **_console log_** el array. Puedes usar este array:
+ */
+//!--------------------------------------------------------------------------------------------------------
+
+placesToTravel = [
+  { id: 5, name: "Japan" },
+  { id: 11, name: "Venecia" },
+  { id: 23, name: "Murcia" },
+  { id: 40, name: "Santander" },
+  { id: 44, name: "Filipinas" },
+  { id: 59, name: "Madagascar" },
+];
+const placesToTravelEdited = []
+
+for (let i = 0; i < placesToTravel.length; i++) {
+  const place = placesToTravel[i]
+  if (place.id !== 11 && place.id !== 40) {
+    placesToTravelEdited.push(place)
+  }
+}
+
+console.log(placesToTravelEdited);
