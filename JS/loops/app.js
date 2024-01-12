ff326fbda272646b850e55df51d31c1c31089897
JuFML <julianafernandezlima@hotmail.com>
2024-01-12 06:39:21 -0300

    Iteración#7

--- JS/loops/app.js
+++ JS/loops/app.js
@@ -203,7 +203,7 @@ const toys = [
  */
 //!--------------------------------------------------------------------------------------------------------
 
-const toys = [
+let toys = [
   { id: 5, name: "Buzz MyYear" },
   { id: 11, name: "Action Woman" },
   { id: 23, name: "Barbie Man" },
@@ -218,3 +218,40 @@ for (const toy of toys) {
 }
 
 console.log(toysFiltred);
+
+/*
+**Iteración #7: For...of avanzado**
+
+Usa un bucle **for...of** para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array:
+
+const popularToys = [];
+const toys = [
+  { id: 5, name: "Buzz MyYear", sellCount: 10 },
+  { id: 11, name: "Action Woman", sellCount: 24 },
+  { id: 23, name: "Barbie Man", sellCount: 15 },
+  { id: 40, name: "El gato con Guantes", sellCount: 8 },
+  { id: 40, name: "El gato felix", sellCount: 35 },
+];
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #7: For...of avanzado**
+ * ? Usa un bucle **for...of** para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array:
+ */
+//!--------------------------------------------------------------------------------------------------------
+
+const popularToys = [];
+toys = [
+  { id: 5, name: "Buzz MyYear", sellCount: 10 },
+  { id: 11, name: "Action Woman", sellCount: 24 },
+  { id: 23, name: "Barbie Man", sellCount: 15 },
+  { id: 40, name: "El gato con Guantes", sellCount: 8 },
+  { id: 40, name: "El gato felix", sellCount: 35 },
+];
+
+for (const toy of toys) {
+  if (toy.sellCount > 15) popularToys.push(toy)
+}
+
+console.log(popularToys);
