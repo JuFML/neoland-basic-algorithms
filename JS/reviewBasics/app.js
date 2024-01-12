cd4770aad76aa5cd5387c3ae4339934d10605101
JuFML <julianafernandezlima@hotmail.com>
2024-01-12 18:19:03 -0300

    Iteración#3

--- JS/reviewBasics/app.js
+++ JS/reviewBasics/app.js
@@ -101,7 +101,7 @@ const users = [
  */
 //!--------------------------------------------------------------------------------------------------------
 
-const users = [
+let users = [
   {
     name: "Manolo el del bombo",
     favoritesSounds: {
@@ -139,7 +139,6 @@ const users = [
 const averageSoundVolumePerUser = {}
 
 for (let user of users) {
-  console.log(user.name)
   averageSoundVolumePerUser[user.name] = 0
 
   for (let sound in user.favoritesSounds) {
@@ -150,3 +149,103 @@ for (let user of users) {
   }
 }
 console.log(averageSoundVolumePerUser);
+
+/*
+**Iteración #3: Mix Fors**
+
+Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos.
+Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
+
+Este ejercicio es un poco complicado con los conocimientos actuales pero...a la vez un buen reto y oportunidad para comprender que hay muchas formas de hacer las cosas en javascript.
+
+const users = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #3: Mix Fors**
+ * ? Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos. Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
+ */
+//!--------------------------------------------------------------------------------------------------------
+
+users = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+const manyTimesSoundAdded = {}
+
+for (let user of users) {
+  for (let sound in user.favoritesSounds) {
+
+    if (!manyTimesSoundAdded[sound]) {
+      manyTimesSoundAdded[sound] = 1
+    } else {
+      manyTimesSoundAdded[sound] += 1
+    }
+  }
+}
+console.log(manyTimesSoundAdded);
