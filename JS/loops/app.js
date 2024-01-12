9ddabbfeeffa7eeb540b0936bcae42bda7f16be5
JuFML <julianafernandezlima@hotmail.com>
2024-01-12 06:35:18 -0300

    Iteración#6

--- JS/loops/app.js
+++ JS/loops/app.js
@@ -181,3 +181,40 @@ for (let i = 0; i < placesToTravel.length; i++) {
 }
 
 console.log(placesToTravelEdited);
+
+/*
+**Iteración #6: Mixed For...of e includes**
+
+Usa un bucle **for...of** para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función **_.includes()_** para comprobarlo.Puedes usar este array:
+
+const toys = [
+  { id: 5, name: "Buzz MyYear" },
+  { id: 11, name: "Action Woman" },
+  { id: 23, name: "Barbie Man" },
+  { id: 40, name: "El gato con Guantes" },
+  { id: 40, name: "El gato felix" },
+];
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #6: Mixed For...of e includes**
+ * ? Usa un bucle **for...of** para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función **_.includes()_** para comprobarlo.Puedes usar este array:
+ */
+//!--------------------------------------------------------------------------------------------------------
+
+const toys = [
+  { id: 5, name: "Buzz MyYear" },
+  { id: 11, name: "Action Woman" },
+  { id: 23, name: "Barbie Man" },
+  { id: 40, name: "El gato con Guantes" },
+  { id: 40, name: "El gato felix" },
+];
+
+const toysFiltred = []
+
+for (const toy of toys) {
+  if (!toy.name.includes("gato")) toysFiltred.push(toy)
+}
+
+console.log(toysFiltred);
