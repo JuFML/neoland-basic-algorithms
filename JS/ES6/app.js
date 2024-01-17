e905165737ae0401ce728ff5a44b960983d6897b
JuFML <julianafernandezlima@hotmail.com>
2024-01-17 05:57:47 -0300

    Iteration#7

--- JS/ES6/app.js
+++ JS/ES6/app.js
@@ -415,3 +415,59 @@ const alienSearched = aliens.find(alien => alien.name === 'Cucushumushu')
 const mutationSearched = mutations.find(mutation => mutation.name === 'Porompompero')
 const alienMutated = { ...alienSearched, ...mutationSearched }
 console.log(alienMutated);
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #7: Reduce**
+ * ? 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de los alumnos usando la función .reduce().
+ */
+//!--------------------------------------------------------------------------------------------------------
+const exams = [
+  { name: 'Yuyu Cabeza Crack', score: 5 },
+  { name: 'Maria Aranda Jimenez', score: 1 },
+  { name: 'Cristóbal Martínez Lorenzo', score: 6 },
+  { name: 'Mercedez Regrera Brito', score: 7 },
+  { name: 'Pamela Anderson', score: 3 },
+  { name: 'Enrique Perez Lijó', score: 6 },
+  { name: 'Pedro Benitez Pacheco', score: 8 },
+  { name: 'Ayumi Hamasaki', score: 4 },
+  { name: 'Robert Kiyosaki', score: 2 },
+  { name: 'Keanu Reeves', score: 10 }
+];
+
+const totalScores = exams.reduce((acc, exam) => {
+  return acc += exam.score
+}, 0)
+
+console.log(totalScores)
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #7: Reduce**
+ * ? 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los alumnos que esten aprobados usando la función .reduce().
+ */
+//!--------------------------------------------------------------------------------------------------------
+
+
+const totalAprovedScores = exams.reduce((acc, exam) => {
+  if (exam.score >= 5) {
+    acc += exam.score
+  }
+  return acc
+}, 0)
+
+console.log(totalAprovedScores)
+
+//!--------------------------------------------------------------------------------------------------------
+/**
+ * ? **Iteración #7: Reduce**
+ * ? 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
+ */
+//!--------------------------------------------------------------------------------------------------------
+
+const averageScores = exams.reduce((acc, exam) => {
+  acc += exam.score / exams.length
+  return acc
+}, 0).toFixed(2)
+
+console.log(averageScores)
