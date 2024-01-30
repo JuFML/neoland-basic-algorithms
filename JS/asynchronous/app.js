b07020260b52ee455d074e227fb63b966a13e56d
JuFML <julianafernandezlima@hotmail.com>
2024-01-30 17:04:57 -0300

    Iteration#2

--- JS/asynchronous/app.js
+++ JS/asynchronous/app.js
@@ -136,3 +136,62 @@ const printElement = (template) => {
   document.querySelector("body").innerHTML += template
   init()
 }
+/*
+**Iteración #2: async-await**
+
+```jsx
+2.1 Gestiona la siguiente promesa para esperar a ejecutar el console usando
+async-await.
+const runTimeOut = () => {
+    const promise = new Promise((resolve) => {
+        setTimeout(function () {
+            resolve();
+        }, 2000);
+    })
+
+    promise.then(() => {console.log('Time out!')})
+};
+
+runTimeOut();
+
+2.2 Convierte la siguiente función con un fetch utilizando async-await.
+Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+function getCharacters () {
+    fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => console.log(characters));
+}
+
+getCharacters();
+
+```
+*/
+
+//!--------------------------------------------------------------------------------------------------------
+//? 2.1 Gestiona la siguiente promesa para esperar a ejecutar el console usando async-await.
+//!--------------------------------------------------------------------------------------------------------
+const runTimeOut = async () => {
+  const promise = new Promise((resolve) => {
+    setTimeout(function () {
+      resolve();
+    }, 2000);
+  });
+
+  try {
+    await promise;
+    console.log('Time out!');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+runTimeOut();
+
+//!--------------------------------------------------------------------------------------------------------
+//? 2.2 Convierte la siguiente función con un fetch utilizando async-await. Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+//!--------------------------------------------------------------------------------------------------------
+async function getCharacters() {
+  const res = await fetch('https://rickandmortyapi.com/api/character')
+  const characters = await res.json()
+  console.log(characters)
+}
+
+getCharacters();
