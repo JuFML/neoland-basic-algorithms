ddd8ac9e8f6e374e1aba16331f47c104b166e637
JuFML <julianafernandezlima@hotmail.com>
2024-02-06 17:49:09 -0300

    Add supermartket and product routes in index.js

--- NODE/creacion del servidor/index.js
+++ NODE/creacion del servidor/index.js
@@ -13,11 +13,16 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: false }));
 
 const UserRoutes = require("./src/api/routes/User.routes")
+app.use("/api/users/", UserRoutes);
+
+const ProductRoutes = require("./src/api/routes/Product.routes");
+app.use("/api/products/", ProductRoutes);
+
+const SupermarketRoutes = require("./src/api/routes/Supermarket.routes");
+app.use("/api/supermarkets/", SupermarketRoutes);
 
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
-
-app.use("/api/users/", UserRoutes);
 
 mongoose.connect(MONGO_URI).then(() => {
   console.log("Banco conectado")
