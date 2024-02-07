9fb6560ac3ed99eed6aacca8d9e6c0843782f5df
JuFML <julianafernandezlima@hotmail.com>
2024-02-07 13:43:28 -0300

    Function getById Supermarket and its route

--- NODE/creacion del servidor/src/api/controllers/Supermarket.controllers.js
+++ NODE/creacion del servidor/src/api/controllers/Supermarket.controllers.js
@@ -110,4 +110,18 @@ const toggleProduct = async (req, res, next) => {
   }
 };
 
-module.exports = { registerSupermarket, toggleProduct }
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const supermarketById = await Supermarket.findById(id);
+    if (supermarketById) {
+      return res.status(200).json(supermarketById);
+    } else {
+      return res.status(404).json("no se ha encontrado el supermercado");
+    }
+  } catch (error) {
+    return res.status(404).json(error.message);
+  }
+};
+
+module.exports = { registerSupermarket, toggleProduct, getById }
--- NODE/creacion del servidor/src/api/routes/Supermarket.routes.js
+++ NODE/creacion del servidor/src/api/routes/Supermarket.routes.js
@@ -1,9 +1,10 @@
 const { upload } = require("../../middleware/files.middleware");
-const { registerSupermarket, toggleProduct } = require("../controllers/Supermarket.controllers");
+const { registerSupermarket, toggleProduct, getById } = require("../controllers/Supermarket.controllers");
 const express = require("express");
 const SupermarketRoutes = express.Router();
 
 SupermarketRoutes.post("/register", upload.single("image"), registerSupermarket);
 SupermarketRoutes.patch("/add/:id", toggleProduct);
+SupermarketRoutes.get("/:id", getById);
 
 module.exports = SupermarketRoutes;
