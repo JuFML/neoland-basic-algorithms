6c6ebb77f9838266b0d07d0e7b8f11fbeee9fdc1
JuFML <julianafernandezlima@hotmail.com>
2024-02-09 10:21:40 -0300

    Add Character model

---
+++ NODE/creacion del servidor/src/api/models/Character.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CharacterSchema = new Schema(
+  {
+    name: { type: String, required: false, unique: false },
+    gender: {
+      type: String,
+      enum: ["hombre", "mujer", "otros"],
+      required: false,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Menssage" }],
+    movies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const Character = mongoose.model("Character", CharacterSchema);
+
+module.exports = Character;
