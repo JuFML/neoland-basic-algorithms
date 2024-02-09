c7dfed77983423aff4bc4d80643f7a8a914db1c2
JuFML <julianafernandezlima@hotmail.com>
2024-02-09 10:47:33 -0300

    Add more functions to User model and its routes

--- NODE/creacion del servidor/src/api/controllers/User.controllers.js
+++ NODE/creacion del servidor/src/api/controllers/User.controllers.js
@@ -5,6 +5,135 @@ const nodemailer = require("nodemailer")
 const bcrypt = require("bcrypt");
 const randomPassword = require("../../utils/randomPassword");
 
+const registerLargo = async (req, res, next) => {
+  let catchImg = req.file?.path;
+
+  try {
+    await User.syncIndexes();
+    let confirmationCode = randomCode();
+    const { email, name } = req.body;
+
+    const userExist = await User.findOne(
+      { email: req.body.email },
+      { name: req.body.name }
+    );
+
+    if (!userExist) {
+      //! -------------LO REGISTRAMOS PORQUE NO HAY COINCIDENCIAS CON UN USER INTERNO
+      const newUser = new User({ ...req.body, confirmationCode });
+
+      if (req.file) {
+        newUser.image = req.file.path;
+      } else {
+        newUser.image = "https://pic.onlinewebfonts.com/svg/img_181369.png";
+      }
+
+      ///! SI HAY UNA NUEVA ASINCRONIA DE CREAR O ACTUALIZAR HAY QUE METER OTRO TRY CATCH
+      try {
+        const userSave = await newUser.save();
+
+        if (userSave) {
+          // ---------------------------> ENVIAR EL CODIGO CON NODEMAILER --------------------
+          const emailEnv = process.env.EMAIL;
+          const password = process.env.PASSWORD;
+
+          const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: emailEnv,
+              pass: password,
+            },
+          });
+
+          const mailOptions = {
+            from: emailEnv,
+            to: email,
+            subject: "Confirmation code",
+            text: `tu codigo es ${confirmationCode}, gracias por confiar en nosotros ${name}`,
+          };
+
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+              return res.status(404).json({
+                user: userSave,
+                confirmationCode: "error, resend code",
+              });
+            }
+            console.log("Email sent: " + info.response);
+            return res.status(200).json({
+              user: userSave,
+              confirmationCode,
+            });
+          });
+        }
+      } catch (error) {
+        return res.status(404).json(error.message);
+      }
+    } else {
+      ///! -------> cuando ya existe un usuario con ese email y ese name
+      if (req.file) deleteImgCloudinary(catchImg);
+      return res.status(409).json("this user already exist");
+    }
+  } catch (error) {
+    if (req.file) deleteImgCloudinary(catchImg);
+    return next(error);
+  }
+};
+
+const register = async (req, res, next) => {
+  let catchImg = req.file?.path;
+  try {
+    await User.syncIndexes();
+    let confirmationCode = randomCode();
+    const { email, name } = req.body;
+
+    const userExist = await User.findOne(
+      { email: req.body.email },
+      { name: req.body.name }
+    );
+    if (!userExist) {
+      const newUser = new User({ ...req.body, confirmationCode });
+      if (req.file) {
+        newUser.image = req.file.path;
+      } else {
+        newUser.image = "https://pic.onlinewebfonts.com/svg/img_181369.png";
+      }
+
+      try {
+        const userSave = await newUser.save();
+
+        if (userSave) {
+          sendEmail(email, name, confirmationCode);
+          setTimeout(() => {
+            if (getTestEmailSend()) {
+              setTestEmailSend(false);
+              return res.status(200).json({
+                user: userSave,
+                confirmationCode,
+              });
+            } else {
+              setTestEmailSend(false);
+              return res.status(404).json({
+                user: userSave,
+                confirmationCode: "error, resend code",
+              });
+            }
+          }, 1100);
+        }
+      } catch (error) {
+        return res.status(404).json(error.message);
+      }
+    } else {
+      if (req.file) deleteImgCloudinary(catchImg);
+      return res.status(409).json("this user already exist");
+    }
+  } catch (error) {
+    if (req.file) deleteImgCloudinary(catchImg);
+    return next(error);
+  }
+};
+
 const registerWithRedirect = async (req, res, next) => {
   let catchImg = req.file?.path;
   try {
@@ -295,4 +424,129 @@ const sendPassword = async (req, res, next) => {
   }
 };
 
-module.exports = { registerWithRedirect, sendCode, checkNewUser, resendCode, login, autoLogin, changePassword, sendPassword }
+const modifyPassword = async (req, res, next) => {
+  console.log("req.user", req.user);
+
+  try {
+    const { password, newPassword } = req.body;
+    const { _id } = req.user;
+
+    if (bcrypt.compareSync(password, req.user.password)) {
+      const newPasswordHashed = bcrypt.hashSync(newPassword, 10);
+
+      try {
+        await User.findByIdAndUpdate(_id, { password: newPasswordHashed });
+
+        const userUpdate = await User.findById(_id);
+
+        if (bcrypt.compareSync(newPassword, userUpdate.password)) {
+          return res.status(200).json({
+            updateUser: true,
+          });
+        } else {
+          return res.status(404).json({
+            updateUser: false,
+          });
+        }
+      } catch (error) {
+        return res.status(404).json(error.message);
+      }
+    } else {
+      return res.status(404).json("password dont match");
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const update = async (req, res, next) => {
+  let catchImg = req.file?.path;
+
+  try {
+    await User.syncIndexes();
+
+    const patchUser = new User(req.body);
+    req.file && (patchUser.image = catchImg);
+
+    patchUser._id = req.user._id;
+    patchUser.password = req.user.password;
+    patchUser.rol = req.user.rol;
+    patchUser.confirmationCode = req.user.confirmationCode;
+    patchUser.email = req.user.email;
+    patchUser.check = req.user.check;
+
+    if (req.body?.gender) {
+      const resultEnum = enumOk(req.body?.gender);
+      patchUser.gender = resultEnum.check ? req.body?.gender : req.user.gender;
+    }
+
+    try {
+      await User.findByIdAndUpdate(req.user._id, patchUser);
+
+      if (req.file) deleteImgCloudinary(req.user.image);
+
+      // ++++++++++++++++++++++ TEST RUNTIME+++++++++++++++++++++++++++++++++++++++
+      const updateUser = await User.findById(req.user._id);
+      const updateKeys = Object.keys(req.body);
+
+      const testUpdate = [];
+      updateKeys.forEach((item) => {
+        if (updateUser[item] === req.body[item]) {
+
+          if (updateUser[item] != req.user[item]) {
+            testUpdate.push({
+              [item]: true,
+            });
+          } else {
+            testUpdate.push({
+              [item]: "sameOldInfo",
+            });
+          }
+        } else {
+          testUpdate.push({
+            [item]: false,
+          });
+        }
+      });
+
+      if (req.file) {
+        updateUser.image === catchImg
+          ? testUpdate.push({
+            image: true,
+          })
+          : testUpdate.push({
+            image: false,
+          });
+      }
+      return res.status(200).json({
+        updateUser,
+        testUpdate,
+      });
+    } catch (error) {
+      if (req.file) deleteImgCloudinary(catchImg);
+      return res.status(404).json(error.message);
+    }
+  } catch (error) {
+    if (req.file) deleteImgCloudinary(catchImg);
+    return next(error);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    const { _id, image } = req.user;
+    await User.findByIdAndDelete(_id);
+
+    if (await User.findById(_id)) {
+      return res.status(404).json("not deleted"); ///
+    } else {
+      deleteImgCloudinary(image);
+      return res.status(200).json("ok delete");
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
+
+module.exports = { registerLargo, register, registerWithRedirect, sendCode, checkNewUser, resendCode, login, autoLogin, changePassword, sendPassword, modifyPassword, update, deleteUser }
--- NODE/creacion del servidor/src/api/routes/User.routes.js
+++ NODE/creacion del servidor/src/api/routes/User.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { upload } = require("../../middleware/files.middleware");
-const { registerWithRedirect, sendCode, checkNewUser, resendCode, login, autoLogin, changePassword, sendPassword } = require("../controllers/User.controllers");
+const { registerWithRedirect, sendCode, checkNewUser, resendCode, login, autoLogin, changePassword, sendPassword, modifyPassword, update, deleteUser, registerLargo, register } = require("../controllers/User.controllers");
+const { isAuth } = require("../../middleware/auth.middleware");
 
 const UserRoutes = express.Router()
 
+UserRoutes.post("/registerLargo", upload.single("image"), registerLargo);
+UserRoutes.post("/registerUtil", upload.single("image"), register);
 UserRoutes.post("/register", upload.single("image"), registerWithRedirect);
 UserRoutes.get("/register/sendMail/:id", sendCode)
 UserRoutes.post("/resend", resendCode);
@@ -12,5 +15,10 @@ UserRoutes.post("/login", login);
 UserRoutes.post("/login/autologin", autoLogin);
 UserRoutes.patch("/forgotpassword", changePassword);
 UserRoutes.patch("/sendPassword/:id", sendPassword);
+
+
+UserRoutes.patch("/changepassword", [isAuth], modifyPassword);
+UserRoutes.patch("/update/update", [isAuth], upload.single("image"), update);
+UserRoutes.delete("/", [isAuth], deleteUser);
 
 module.exports = UserRoutes
